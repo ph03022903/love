e6b56b4ea981cc272c340445e21de09d9ae78b1f
ph03022903 <phuocndtkw@gmail.com>
2022-12-05 19:36:28 +0700

    Updates

---
+++ precache-manifest.654d8b872245b998d080d4b1f4b46f74.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e378cbd255abd20d88a",
+    "url": "/love/static/css/main.5e02e4ae.chunk.css"
+  },
+  {
+    "revision": "1e378cbd255abd20d88a",
+    "url": "/love/static/js/main.1e378cbd.chunk.js"
+  },
+  {
+    "revision": "5cc56db2f5898495bd5b",
+    "url": "/love/static/js/1.5cc56db2.chunk.js"
+  },
+  {
+    "revision": "46f6a4a3dbcc486c6913",
+    "url": "/love/static/js/2.46f6a4a3.chunk.js"
+  },
+  {
+    "revision": "cc29fbfcb5687b666c2f",
+    "url": "/love/static/js/runtime~main.cc29fbfc.js"
+  },
+  {
+    "revision": "93a85fe4ff15ec7bcd3f30d1db03c4b1",
+    "url": "/love/static/media/anh.93a85fe4.PNG"
+  },
+  {
+    "revision": "a3697c5551ca72983147b30231122c69",
+    "url": "/love/static/media/em1.a3697c55.png"
+  },
+  {
+    "revision": "971f14ff2aede65ec4bc6a1a1cafe222",
+    "url": "/love/static/media/I-Do-911.971f14ff.mp3"
+  },
+  {
+    "revision": "2a4f79c062ffe4924cb3851b2defb9f7",
+    "url": "/love/static/media/She-Neva-Knows-JustaTee.2a4f79c0.mp3"
+  },
+  {
+    "revision": "e1e5b1500f6f44e65478230e4c8fe5e8",
+    "url": "/love/static/media/1.e1e5b150.JPG"
+  },
+  {
+    "revision": "2f005e53158775d5136fde2e65942f2a",
+    "url": "/love/static/media/2.2f005e53.JPEG"
+  },
+  {
+    "revision": "78fe2c07878cbd3f43dfde6340a19340",
+    "url": "/love/static/media/3.78fe2c07.JPG"
+  },
+  {
+    "revision": "bf30ad765821f79bc7fbc66c6699fa33",
+    "url": "/love/static/media/4.bf30ad76.JPG"
+  },
+  {
+    "revision": "6b9f6f1d44e2ca3abe9705451d16fa86",
+    "url": "/love/static/media/5.6b9f6f1d.JPG"
+  },
+  {
+    "revision": "09f2db42d288769fc9e909b903dcd262",
+    "url": "/love/static/media/6.09f2db42.JPG"
+  },
+  {
+    "revision": "f22efd54014b81144ae822607f38a989",
+    "url": "/love/static/media/7.f22efd54.JPG"
+  },
+  {
+    "revision": "e2820d2d411a2d226354e8119bb539b1",
+    "url": "/love/static/media/8.e2820d2d.JPG"
+  },
+  {
+    "revision": "8c2b19f8f41273337190ddcfdb06cd39",
+    "url": "/love/static/media/9.8c2b19f8.JPG"
+  },
+  {
+    "revision": "afa61d2120776f2f9f99607f5742f971",
+    "url": "/love/static/media/10.afa61d21.JPG"
+  },
+  {
+    "revision": "6d3ab2ffa8085f0d5805ab31b567c909",
+    "url": "/love/static/media/11.6d3ab2ff.JPG"
+  },
+  {
+    "revision": "abe8e388395d9dbdfcc05f05ed28e82d",
+    "url": "/love/static/media/12.abe8e388.JPG"
+  },
+  {
+    "revision": "54578e1e64a5d8ac8b43c78e2256c600",
+    "url": "/love/static/media/bg1.54578e1e.JPG"
+  },
+  {
+    "revision": "ca8383cde8b27fcbc9af461fe4f9cc7c",
+    "url": "/love/static/media/bg2.ca8383cd.JPG"
+  },
+  {
+    "revision": "8ab971e7f323b019f01a7314eb4c6ee5",
+    "url": "/love/static/media/bg3.8ab971e7.JPG"
+  },
+  {
+    "revision": "8eb9cfd57d6290f9fb5d89083835be71",
+    "url": "/love/static/media/love.8eb9cfd5.JPEG"
+  },
+  {
+    "revision": "8c973cf7db081202d04a377561ecc04f",
+    "url": "/love/static/media/tim.8c973cf7.png"
+  },
+  {
+    "revision": "9b53b80a10acf38cc6f9f0190e8069bc",
+    "url": "/love/static/media/play.9b53b80a.svg"
+  },
+  {
+    "revision": "1951cd9ae41c6e5eeed39a5004ff437e",
+    "url": "/love/static/media/pause.1951cd9a.svg"
+  },
+  {
+    "revision": "225acb10b17c4d98fecab78edfdf3bfb",
+    "url": "/love/static/media/next.225acb10.svg"
+  },
+  {
+    "revision": "b63de741124302ce689b4c4f204084d3",
+    "url": "/love/static/media/prev.b63de741.svg"
+  },
+  {
+    "revision": "5d69a2aa35ee1bac0f75675556083429",
+    "url": "/love/index.html"
+  }
+];
